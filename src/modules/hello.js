f240f78d56d653bcb614dee3170c625a60d0ab8b
arunas.kirgetovas <arunas.kirgetovas@stud.kitm.lt>
2024-05-09 15:46:39 +0300

    initial commit

---
+++ src/modules/hello.js
@@ -0,0 +1,32 @@
+// const hello = ()=> {
+//     let h1 = document.createElement("h1");
+//     h1.textContent = "labas";
+//     document.querySelector("body").appendChild(h1)
+// }
+
+// export default hello
+
+
+//------------------------------------------------------------------------------------
+
+
+// const messages = [
+//     {
+//         id:1,
+//         imgUrl: 'https://learn.microsoft.com/en-us/windows/images/devhome-welcome.png',
+//         message: "Some text"
+//     },
+//     {
+//         id:1,
+//         imgUrl: 'https://learn.microsoft.com/en-us/windows/images/devhome-welcome.png',
+//         message: "Some text"
+//     },
+//     {
+//         id:1,
+//         imgUrl: 'https://learn.microsoft.com/en-us/windows/images/devhome-welcome.png',
+//         message: "Some text"
+//     }
+// ]
+const messageButton = document.getElementById("messageButton");
+messageButton.addEventListener("click")
+
